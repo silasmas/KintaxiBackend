1dfd90aeb0cf94334ce378f95df4879c8849a8c3
xsamtech <1496973+xanderssamoth@users.noreply.github.com>
2025-01-10 07:25:22 +0100

    Update RoleController.php

--- app/Http/Controllers/Web/RoleController.php
+++ app/Http/Controllers/Web/RoleController.php
@@ -115,10 +115,10 @@ class RoleController extends BaseController
             $user_data = $user_resource->toArray(request());
             $vehicles_collection = Vehicle::orderByDesc('created_at')->get();
             $vehicles_data = ResourcesVehicle::collection($vehicles_collection)->toArray(request());
-            $countries_collection = Country::all();
+            $countries_collection = Country::find(1);
             $countries_data = ResourcesCountry::collection($countries_collection)->toArray(request());
 
-            dd($countries_collection);
+            dd($countries_data);
             return view('role', [
                 'entity' => $entity,
                 'user' => $user_data,
